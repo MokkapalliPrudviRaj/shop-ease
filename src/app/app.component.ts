4557ae957606e438714778e7381ffab8e186c963
Thiru <pthirumalayadav@gmail.com>
2025-01-27 21:38:41 +0530

    Filter and sort

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +8,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Product Search App';
-  products: any[] = []; 
+  products: any[] = [];
+  brands: Set<any> = new Set();
+  selectedBrands: Set<any> = new Set();
+  filteredProducts: any[] = [];
+
+
+  ratingSort(isAscending: boolean) {
+    if (isAscending) {
+      this.products.sort((a, b) => a.rating - b.rating);
+      return;
+    }
+    this.products.sort((a, b) => b.rating - a.rating);
+  }
+
+  onCheckboxChange(event: Event, brand: string): void {
+    const checkbox = event.target as HTMLInputElement;
+    if (checkbox.checked) {
+      this.selectedBrands.add(brand);
+      this.filteredProducts = this.products.filter(product => this.selectedBrands.has(product?.brand));
+      return;
+    }
+
+    this.selectedBrands.delete(brand);
+    if (this.selectedBrands.size === 0) {
+      this.filteredProducts = this.products;
+    } else {
+      this.filteredProducts = this.products.filter(product => this.selectedBrands.has(product?.brand));
+    }
+  }
 
   onProductsChange(products: any[]): void {
     this.products = products;
+    this.filteredProducts = products;
+    this.brands = new Set(products.map(a => a.brand));
   }
 }
